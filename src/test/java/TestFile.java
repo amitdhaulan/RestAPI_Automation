19ff34506862f85c23033f194b3c6063f69900f9
Amit <amitkumar025@gmail.com>
2022-05-13 22:50:38 +0530

    testing

---
+++ src/test/java/TestFile.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestFile {
+    @Test(priority = 0)
+    public void getRequestWithoutQueryParameter() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+        Response response = given().log().all()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/posts")
+                .then().log().all()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("qui est esse", response.jsonPath().getString("title[1]"));
+    }
+}
